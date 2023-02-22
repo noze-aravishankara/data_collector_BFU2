d9ca5253e14824acb50d9004a8a3a91fa15b6708
noze-aravishankara <aravishankara@noze.ca>
2023-02-21 23:35:45 -0500

    Made working BFU2 Code

--- log/plotter.py
+++ log/plotter.py
@@ -8,7 +8,7 @@ from os import getcwd, listdir
 from os.path import isdir
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 
@@ -17,8 +17,25 @@ class Plotter:
         self.data = [pd.read_csv(f) for f in file_array]
         logging.info([f'Test shape: {df.shape}' for df in self.data])
 
+        self.fig = go.Figure()
+
     def baseline_data(self, arr):
         pass
+
+    def gen_figure(self, data):
+        for element in data:
+            logging.info(element)
+            self.fig.add_trace(
+                go.Scatter(
+                    visible=False,
+                    line=dict(color="#00CED1", width=3),
+                    name=element,
+                    y=A[element],
+                ),
+            )
+
+    def show_figure(self):
+        self.fig.show()
 
 
 if __name__ == '__main__':
@@ -28,5 +45,6 @@ if __name__ == '__main__':
     BFU2_test = ['humidity_data_BFU2/230220_1544_PTFE-HUM-T002-V2-F10-R1/230220_1544_PTFE-HUM-T002-V2-F10-R1_BFU1.csv',
                  'humidity_data_BFU2/230220_1550_PTFE-HUM-T002-V2-F10-R2/230220_1550_PTFE-HUM-T002-V2-F10-R2_BFU2.csv']
 
-    A = Plotter(BFU1_test)
-    print(A.data[0].head())
+    A = Plotter(BFU2_test)
+    A.gen_figure(A.data)
+    A.show_figure()
