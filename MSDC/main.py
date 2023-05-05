8ca10561062502aeaeb306fe46954f9380099a20
noze-aravishankara <aravishankara@noze.ca>
2023-05-05 07:47:52 -0400

    Minor Changes

--- MSDC/main.py
+++ MSDC/main.py
@@ -9,7 +9,7 @@ import numpy as np
 import serial
 import json
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 from CONFIG.config_parser import config_parser
 from data_manager import data_manager
@@ -21,7 +21,8 @@ class multi_data_collector:
         self.run_status = False
         self.create_data_folder()
         self.device_setup()
-        self.single_time_listener()
+        #self.single_time_listener()
+        self.thread_handler()
 
     def folder_info(self):
         self.now = dt.now()
@@ -46,12 +47,13 @@ class multi_data_collector:
             sys.exit()
 
     def device_setup(self):
-        #self.device = serial.serial(self._config["devices"]["INTERNAL_SERVER"]["com_port"], self._config["devices"]["INTERNAL_SERVER"]["baud_rate"])
+        self.device = serial.Serial(self._config["devices"]["INTERNAL_SERVER"]["com_port"], self._config["devices"]["INTERNAL_SERVER"]["baud_rate"])
         self.dms = {}
         for sensor in self._config["devices"]["SENSORS"]:
             self.dms[sensor] = data_manager(f'{self.directory}/{self.now_}_{self._config["output_file_prefix"]}_{sensor}.csv')
 
     def thread_handler(self):
+        self.run_status = True
         self.thread = threading.Thread(target=self.continuous_multi_listener)
         self.thread.start()
 
@@ -60,11 +62,10 @@ class multi_data_collector:
             try:
                 if self.device.in_waiting > 0:
                     _ = json.loads(str(self.device.readline().decode("utf-8")))
-                    for sensor in _:
-                        _2 = threading.Thread(target=self.dms[sensor].append_file, args=(_['t'],))
-                        _2.start()
-                else:
-                    continue
+                    logging.debug('Got New Data')
+                    # for sensor in _:
+                    #     _2 = threading.Thread(target=self.dms[sensor].append_file, args=(_['t'],))
+                    #     _2.start()
             except KeyboardInterrupt:
                 logging.info("--- ENDING DATA COLLECTION ---")
                 self.thread.join()
@@ -72,13 +73,34 @@ class multi_data_collector:
                 sys.exit()
 
     def single_time_listener(self):
-        while not self.device.in_waiting > 0:
-            time.sleep(0.1)
-        _data = json.loads(str(self.device.readline().decode("utf-8")))
-        keys = self.update_file_with_headers(_data["dev1"])
-        for sensor in self.dms:
-            self.dms[sensor].append_file(keys)
-        logging.info('Done Updating Headers')
+        headers = False
+        self.device.flush()
+        logging.debug('Initializing the headers, and flush')
+        while self.wait_for_data() == False:
+            logging.info('Waiting for data')
+            time.sleep(0.01)
+        logging.info('Exiting loop')
+        _ = str(self.device.readline().decode("utf-8"))
+        logging.debug('Got the data')
+
+        # while not headers:
+        #     logging.debug('inside while loop 1')
+        #     if self.device.in_waiting > 0:
+        #         _data = json.loads(str(self.device.readline().decode("utf-8")))
+        #         keys = self.update_file_with_headers(_data["dev1"])
+        #         for sensor in self.dms:
+        #             self.dms[sensor].append_file(keys)
+        #         logging.info('Done Updating Headers')
+        #         headers = True
+        #     else:
+        #         logging.debug('Waiting for headers')
+
+    def wait_for_data(self):
+        if self.device.in_waiting > 0:
+            return True
+        else:
+            return False
+
 
     def update_file_with_headers(self, data):
         _ = self.get_new_data()
